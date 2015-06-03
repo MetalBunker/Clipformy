097461e31fe0e26e52485a815413391e15fcb5e5
LucasMetal <lucasmetal@hotmail.com>
2015-06-03 02:47:45 -0300

    - First code commit!

---
+++ Clipformy/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Clipformy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTransform = new System.Windows.Forms.Button();
+            this.txtTarget = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTransform
+            // 
+            this.btnTransform.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTransform.Enabled = false;
+            this.btnTransform.Location = new System.Drawing.Point(626, 22);
+            this.btnTransform.Name = "btnTransform";
+            this.btnTransform.Size = new System.Drawing.Size(78, 23);
+            this.btnTransform.TabIndex = 0;
+            this.btnTransform.Text = "Transform!";
+            this.btnTransform.UseVisualStyleBackColor = true;
+            this.btnTransform.Click += new System.EventHandler(this.btnTransform_Click);
+            // 
+            // txtTarget
+            // 
+            this.txtTarget.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTarget.Location = new System.Drawing.Point(12, 25);
+            this.txtTarget.Name = "txtTarget";
+            this.txtTarget.Size = new System.Drawing.Size(608, 20);
+            this.txtTarget.TabIndex = 1;
+            this.txtTarget.TextChanged += new System.EventHandler(this.txtTarget_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Format:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(494, 431);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(207, 13);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://github.com/MetalBunker/Clipformy";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(494, 453);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(129, 13);
+            this.linkLabel2.TabIndex = 8;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "https://www.transformy.io";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 51);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.txtSource);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.txtResult);
+            this.splitContainer1.Size = new System.Drawing.Size(692, 377);
+            this.splitContainer1.SplitterDistance = 337;
+            this.splitContainer1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Source:";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSource.Location = new System.Drawing.Point(3, 16);
+            this.txtSource.Multiline = true;
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(331, 358);
+            this.txtSource.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Results:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Location = new System.Drawing.Point(3, 16);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(345, 358);
+            this.txtResult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 474);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTarget);
+            this.Controls.Add(this.btnTransform);
+            this.Name = "Form1";
+            this.Text = "Clipformy";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTransform;
+        private System.Windows.Forms.TextBox txtTarget;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ Clipformy/Form1.cs
@@ -0,0 +1,92 @@
+﻿using RestSharp;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Clipformy
+{
+    public partial class Form1 : Form
+    {
+        private RestClient _transformyRestClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _transformyRestClient = new RestClient("https://www.transformy.io/api/transform");
+        }
+
+        #region Form methods
+
+        private void btnTransform_Click(object sender, EventArgs e)
+        {
+            // TODO: Add some UI notification of the request going on
+            // TODO: Make the request non ui-blocking
+            txtResult.Text = Transform(txtSource.Text, txtTarget.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!TryToFillFromClipboard()) return;
+
+            btnTransform_Click(null, null);
+            Clipboard.SetText(txtResult.Text);
+            Console.Beep();
+            Application.Exit();
+        }
+
+        private void txtTarget_TextChanged(object sender, EventArgs e)
+        {
+            btnTransform.Enabled = txtTarget.Text.Length > 0;
+        }
+
+        #endregion Form methods
+
+        #region Private methods
+
+        /// <summary>
+        /// Tries to fill the controls of the form from the data available in the clipboard (if any).
+        /// The data must be in an specific format in order to be understood, it expects 1 line of text (target), 
+        /// followed by two blank lines (can contain tabs or spaces), and 1 or more lines of text (source).
+        /// Ie:
+        ///     Lucas is 31 years old!
+        ///
+        ///
+        ///     Lucas 31
+        ///     Mauro 30
+        /// </summary>
+        /// <returns>true if there was data in the clipboard and it mached the required format.</returns>
+        private bool TryToFillFromClipboard()
+        {
+            if (!Clipboard.ContainsText()) return false;
+
+            var clipboardText = Clipboard.GetText();
+            var lines = clipboardText.Split(new[] {Environment.NewLine}, StringSplitOptions.None);
+
+            // Very nasty format checking :P
+            if (lines.Count() >= 4
+                && lines[0].Trim().Length > 0
+                && lines[1].Trim(' ', '\n').Length == 0
+                && lines[2].Trim(' ', '\n').Length == 0)
+            {
+                txtTarget.Text = lines[0];
+                txtSource.Text = string.Join(Environment.NewLine, lines.Skip(3));
+                return true;
+            }
+
+            txtSource.Text = clipboardText;
+            return false;
+        }
+
+        private string Transform(string source, string target)
+        {
+            var request = new RestRequest(Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(new { source, target });
+
+            // Transformy returns "\n" for new lines, we need to replace those
+            return _transformyRestClient.Execute(request).Content.Replace("\n", Environment.NewLine);
+        }
+
+        #endregion Private methods
+    }
+}
